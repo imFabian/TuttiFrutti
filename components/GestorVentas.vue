1e83a4ee18490ccebcb8479fb094ec0ff49b8c8c
Fabian Riveros <fabian.riveros02@inacapmail.cl>
2022-12-05 21:07:39 -0300

    manejo de errores cuando cancela la compra

--- components/GestorVentas.vue
+++ components/GestorVentas.vue
@@ -11,10 +11,13 @@
                 <h5>Listado de ventas <small style="font-size:14px !important;">({{listadoVentas.length}})</small></h5>
                 <div v-for="(item, index) in listadoVentas" :key="index" class="w-100 mb-2 p-2 row" style="border-radius: 10px;border-bottom: 1px solid grey;">
                     <div class="col-4"><span style="font-weight:bold;">{{item.rut}} {{item.nombre}} {{item.apellido}}</span></div>
-                    <div class="col row justify-content-between">
+                    <div class="col row justify-content-between" v-if="(item.transactionStatus != undefined)">
                         <span class="col" :style="item.transactionStatus.status == 'AUTHORIZED' ? 'color:green;font-weight:bold;' : 'color:red;font-weight:bold;'">{{item.transactionStatus.status}}</span>
                         <span class="col">Total:${{item.transactionStatus.amount}}</span>
                         <span class="col">Fecha:{{item.transactionStatus.transaction_date}}</span>
+                    </div>
+                    <div v-else>
+                        <span class="col" style="color:red;font-weight:bold">CANCELED</span>
                     </div>
                     <div class="col-12 text-center">
                         Direccion de entrega: <span style="font-weight:bold;">{{item.direccion}}</span>
@@ -33,6 +36,7 @@ export default {
     },
     async mounted(){
         this.listadoVentas = await this.$store.dispatch('getAllSalesDB');
+        // console.log(await this.$store.dispatch('getAllSalesDB'));
     }
 }
 </script>
